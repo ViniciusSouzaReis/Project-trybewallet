08df1337089c86a05a3700569633705ad6f7fe1a
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-29 14:24:07 -0300

    Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
+  findInfoCurrency = (param, paramtwo) => {
+    const newArray = param.map((element) => Object.values(element.exchangeRates));
+    if (newArray.length >= 1) {
+      const findName = newArray[0].find((element) => element.code === paramtwo);
+      return ({
+        cursiveName: findName.name,
+        ask: findName.ask,
+      });
+    }
+  };
+
   render() {
+    const { expenses } = this.props;
+    const result = expenses.map((element) => this
+      .findInfoCurrency(expenses, element.currency));
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            { expenses.map((e, index) => (
+              <tr key={ index }>
+                <td>{e.description}</td>
+                <td>{e.tag}</td>
+                <td>{e.method}</td>
+                <td>{`${e.value}.00`}</td>
+                <td>{result[index].cursiveName}</td>
+                <td>{Number(result[index].ask).toFixed(2)}</td>
+                <td>{(e.value * result[index].ask).toFixed(2)}</td>
+                <td>Real</td>
+              </tr>
+            ))}
+
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -8,31 +9,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <WalletForm />
-        <table>
-          <thead>
-            <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td />
-              <td />
-            </tr>
-            <tr>
-              <td />
-              <td />
-            </tr>
-          </tbody>
-        </table>
+        <Table />
       </div>
     );
   }
